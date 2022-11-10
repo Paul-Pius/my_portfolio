60ce1f3638a82375a5a3f3e27c1ca64adbed14a7
yoppeep <paulpius55@gmail.com>
2022-11-11 00:10:05 +0100

    my portfolio commit

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import Shoppingcart from '../assets/projects/shoppingcart.png'
+import Scorecard from '../assets/projects/scorecard.png'
+import Ecopal from '../assets/projects/ecopal.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Scorecard",
+        image:Scorecard,
+        github:"https://github.com/decadevs/live-project-score-card-sq011a-group-2-node_group_2_live_project.git",
+        live:"https://astonishing-souffle-b0fb04.netlify.app/login",
+    },
+    {
+        id:2,
+        name:"Shopping cart",
+        image:Shoppingcart,
+        github:"https://github.com/Paul-Pius/shopping_cart.git",
+        live:"https://paul-shopping-cart.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Ecopal",
+        image:Ecopal,
+        github:"https://github.com/yopeepp/Ecopal.git",
+        live:"https://ecopal.herokuapp.com",
+    },
+
+
+]
